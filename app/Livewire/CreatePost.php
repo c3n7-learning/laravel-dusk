963d40d515db017ab214f9af4ff95b0abd2e151a
Timothy Karani <kartimothy@gmail.com>
2024-12-12 20:15:59 +0300

    Can create post

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
+    public ?string $title;
+    public ?string $content;
+
+    public array $rules = [
+        'title' => 'required|string',
+        'content' => 'required|string',
+    ];
+
     public function render()
     {
         return view('livewire.create-post');
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        Post::create([
+            'title' => $this->title,
+            'content' => $this->content,
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('post.index')->with('success_message', 'Post was successfully created.');
     }
 }
--- resources/views/livewire/list-posts.blade.php
+++ resources/views/livewire/list-posts.blade.php
@@ -8,12 +8,8 @@
     <div class="py-12 text-gray-900 dark:text-gray-100">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-
                 @if (session('success_message'))
-                    <div class="alert alert-success">
+                    <div class="p-6 mt-2 font-medium text-sm text-green-600 dark:text-green-400">
                         {{ session('success_message') }}
                     </div>
                 @endif
